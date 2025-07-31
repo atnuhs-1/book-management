5144373c63b5782bbb5252cd946c79a49d83080c
hamuhamu5hamu <fujisyo5090@au.com>
2025-07-31 16:41:53 +0900

    add

--- backend/app/schemas/__init__.py
+++ backend/app/schemas/__init__.py
@@ -1,3 +1,4 @@
 # app/schemas/__init__.py
-from .book import BookCreate, BookOut,BookUpdate
-from .user import Token, UserCreate, UserOut,TokenWithUser
+from .book import BookCreate, BookOut, BookUpdate
+from .user import Token, UserCreate, UserOut, TokenWithUser
+from .food_item import FoodItemCreate, FoodItemRead  # ✅ 追加
